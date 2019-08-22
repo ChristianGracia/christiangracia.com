cd7b96db6effe9341e6b21525d45e0e7cb019ea3
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-21 21:28:29 -0400

    update

---
+++ site/build/precache-manifest.b97d08b5bf852c96a1502e1d6536a3d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01953307e973c76b68906565381187c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f72342ec182286b0dd2",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5f14e05d6481ec4e9199",
+    "url": "/static/js/2.7d531e2e.chunk.js"
+  },
+  {
+    "revision": "2f72342ec182286b0dd2",
+    "url": "/static/js/main.86e952b9.chunk.js"
+  },
+  {
+    "revision": "b3e07a0940fb99869112",
+    "url": "/static/js/runtime~main.6f3e6252.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
