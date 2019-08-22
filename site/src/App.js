7e1be001f8ae16d8373899142d4541c1a4ffdd9a
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-21 21:23:50 -0400

    got react working on site

--- site/src/App.js
+++ site/src/App.js
@@ -1,35 +1,26 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="all" style={{ backgroundColor: "#222" }}>
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Christian Gracia</h2>
-          </div>
-          <p className="App-intro">
-            <div>
-              <img src="https://pbs.twimg.com/profile_images/997619453113327618/PV-7rdJG_400x400.jpg" />
-            </div>
-          </p>
-          <p
-            style={{
-              fontSize: 40,
-              fontWeight: "bold",
-              color: "white",
-              height: 100
-            }}
-          >
-            Coming Soon
-          </p>
-        </div>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
