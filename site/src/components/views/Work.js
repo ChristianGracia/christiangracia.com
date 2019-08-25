3078dadbe94323663f33f5485255d97839aceb59
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-25 15:53:25 -0400

    added animation

--- site/src/components/views/Work.js
+++ site/src/components/views/Work.js
@@ -5,46 +5,53 @@ import { Button } from "react-bootstrap";
 
 export default class Work extends Component {
   state = {
-    showGithub: false
+    showGithub: false,
+    showRunner: true
   };
 
   componentDidMount() {
     setTimeout(() => {
-      this.setState({ showGithub: true });
+      this.setState({ showGithub: true, showRunner: false });
     }, 2000);
   }
   render() {
     return (
       <div style={{}}>
         <div style={{ padding: 20 }}> </div>
-        <Fade left>
-          <div
-            className="container"
-            style={{
-              alignItems: "center",
-              textAlign: "center",
-              justifyContent: "center"
-            }}
-          >
-            <p style={{ fontSize: 70, textAlign: "center", color: "yellow" }}>
-              <i class="fas fa-running"></i>
-            </p>
 
-            <p
-              style={{
-                color: "white",
-                textAlign: "center",
-                margin: "auto"
-              }}
-            >
-              I have built numerous full stack applications and worked with many
-              different languages, libraries, frameworks, and data structures.
-              Built this site on a virtual ubuntu server from scratch and am
-              hosting it myself. Currently mostly working with React, React
-              Native, and C#.
-            </p>
-          </div>
-        </Fade>
+        <div
+          className="container"
+          style={{
+            alignItems: "center",
+            textAlign: "center",
+            justifyContent: "center"
+          }}
+        >
+          <Fade top>
+            <Fade left opposite when={this.state.showRunner}>
+              <p style={{ fontSize: 70, textAlign: "center", color: "yellow" }}>
+                <i class="fas fa-running"></i>
+              </p>
+            </Fade>
+          </Fade>
+          <Fade left>
+            <div>
+              <p
+                style={{
+                  color: "white",
+                  textAlign: "center",
+                  margin: "auto"
+                }}
+              >
+                I have built numerous full stack applications and worked with
+                many different languages, libraries, frameworks, and data
+                structures. Built this site on a virtual ubuntu server from
+                scratch and am hosting it myself. Currently mostly working with
+                React, React Native, and C#.
+              </p>
+            </div>
+          </Fade>
+        </div>
 
         <div
           style={{
