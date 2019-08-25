005487535486bc4808ec7894236621a9d2d4340c
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-25 12:11:38 -0400

    routes working again

---
+++ site/build/precache-manifest.6b83ec19160d22ba3c40bb40b79eaac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3dd25d99bba10affbaedb5fec689b2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "181527b8d1b8b3f163cf",
+    "url": "/static/css/main.3a02a614.chunk.css"
+  },
+  {
+    "revision": "c4b785b604629a36ca71",
+    "url": "/static/js/2.5aa8d51d.chunk.js"
+  },
+  {
+    "revision": "181527b8d1b8b3f163cf",
+    "url": "/static/js/main.96ae95ee.chunk.js"
+  },
+  {
+    "revision": "b3e07a0940fb99869112",
+    "url": "/static/js/runtime~main.6f3e6252.js"
+  },
+  {
+    "revision": "56a238d170387fb53ad9d6c8f4b4923f",
+    "url": "/static/media/background.56a238d1.png"
+  }
+]);
