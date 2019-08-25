60888023723c4622968231a3fb655201cceb3072
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-25 08:36:53 -0400

    about page set up

--- site/src/components/Content.js
+++ site/src/components/Content.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from "react";
 import Fade from "react-reveal/Fade";
+import LightSpeed from "react-reveal/LightSpeed";
 import Skills from "./cards/Skills";
 import Education from "./cards/Education";
 
@@ -19,10 +20,35 @@ export default class Content extends Component {
               <Fade top>
                 <Skills />
               </Fade>
+
+              <LightSpeed top cascade>
+                <div>
+                  <p style={{ color: "white" }}>
+                    <span style={{ color: "#00FF00" }}>Languages:</span>{" "}
+                    JavaScript, C, Python, PHP, HTML/CSS
+                  </p>
+                  <p style={{ color: "white" }}>
+                    <span style={{ color: "#00FF00" }}>Frameworks:</span> React
+                    Native, VueJS
+                  </p>
+                  <p style={{ color: "white" }}>
+                    <span style={{ color: "#00FF00" }}>Libraries:</span>{" "}
+                    ReactJS, Redux
+                  </p>
+                  <span style={{ marginTop: 20 }}></span>
+                  <p style={{ color: "white" }}>
+                    MongoDB, MERN Stack apps, REST API, GraphQL, NodeJS, Django,
+                    Bootstrap,
+                  </p>
+                  <p style={{ color: "white" }}>
+                    Agile Development Methodogy, Linux/Ubuntu , Server hosting
+                  </p>
+                </div>
+              </LightSpeed>
             </div>
             <div style={{ padding: 10 }}></div>
             <div className="col-sm">
-              <Fade top>
+              <Fade right>
                 <Education />
               </Fade>
             </div>
--- site/src/components/cards/Skills.js
+++ site/src/components/cards/Skills.js
@@ -1,61 +1,59 @@
 import React, { Component } from "react";
-import { Card } from "react-bootstrap";
+import Fade from "react-reveal/Fade";
 
 export default class Skills extends Component {
   render() {
     return (
       <div style={{ width: "", padding: 10 }}>
-        <h1 style={{ textAlign: "center", color: "white", paddingBottom: 20 }}>
+        <h1 style={{ textAlign: "center", color: "white", paddingBottom: 40 }}>
           Skills{" "}
           <span>
             <i class="fas fa-hammer"></i>
           </span>
         </h1>
 
-        <div style={{ textAlign: "center", fontWeight: "bold" }}>
+        <div
+          style={{ textAlign: "center", fontWeight: "bold", color: "white" }}
+        >
           <h1
             style={{
-              fontSize: 37,
+              fontSize: 30,
               justifyContent: "center",
               textAlign: "center",
               alignItems: "center",
-              fontWeight: "bold",
-
-              paddingBottom: 15
+              fontWeight: "bold"
             }}
           >
-            {" "}
-            Android
-            <span>
-              <i class="fab fa-android"></i>
-            </span>{" "}
-            / iOS {"  "}
-            <span>
+            <span style={{}}>
               {" "}
-              <i class="fab fa-apple"></i>
-            </span>{" "}
-            App Development
+              Android
+              <span
+                style={{
+                  color: "#00FF00",
+                  marginLeft: 6,
+                  paddingRight: 2,
+                  fontSize: 40
+                }}
+              >
+                <i class="fab fa-android"></i>
+              </span>
+              {"  "}/ iOS {"  "}
+              <span
+                style={{
+                  color: "white",
+                  paddingLeft: 2,
+                  paddingRight: 2,
+                  fontSize: 40
+                }}
+              >
+                {"  "}
+                <i class="fab fa-apple"></i>
+              </span>
+              {"  "}
+              App Development
+            </span>
           </h1>
-
-          <p style={{ color: "white" }}>
-            <span style={{ color: "#00FF00" }}>Languages:</span> JavaScript, C,
-            Python, PHP, HTML/CSS
-          </p>
-          <p style={{ color: "white" }}>
-            <span style={{ color: "#00FF00" }}>Frameworks:</span> React Native,
-            VueJS
-          </p>
-          <p style={{ color: "white" }}>
-            <span style={{ color: "#00FF00" }}>Libraries:</span> ReactJS, Redux
-          </p>
-          <span style={{ marginTop: 20 }}></span>
-          <p style={{ color: "white" }}>
-            MongoDB, MERN Stack apps, REST API, GraphQL, NodeJS, Django,
-            Bootstrap,
-          </p>
-          <p style={{ color: "white" }}>
-            Agile Development Methodogy, Linux/Ubuntu , Server hosting
-          </p>
+          <div style={{ paddingBottom: 20 }}></div>
         </div>
       </div>
     );
