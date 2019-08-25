ee0d1dbd1dd9df3152e421741d888aeec9ec3e70
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-25 07:59:16 -0400

    setting up about page

--- site/src/components/cards/Education.js
+++ site/src/components/cards/Education.js
@@ -1,27 +1,26 @@
 import React, { Component } from "react";
-import { ListGroup } from "react-bootstrap";
 
 export default class Education extends Component {
   render() {
     return (
       <div style={{}}>
-        <h1 style={{ textAlign: "center", color: "white" }}>
+        <h1 style={{ textAlign: "center", color: "white", paddingBottom: 20 }}>
           Education <i class="fas fa-graduation-cap"></i>
         </h1>
 
-        <ListGroup style={{}}>
-          <ListGroup.Item>
+        <div style={{ textAlign: "center" }}>
+          <p style={{ color: "white" }}>
             Career Devs Computer Science University 2018-2019{"\n"}
             Full Stack Web Development{"\n"}
             <a href="http://careerdevs.com/">Careerdevs.com</a>
-          </ListGroup.Item>
-
-          <ListGroup.Item>
+          </p>
+          <p style={{ color: "white" }}>
+            {" "}
             Rhode Island College{"\n"}
             BS in General Management
             {"\n\n"}2012-2016
-          </ListGroup.Item>
-        </ListGroup>
+          </p>
+        </div>
       </div>
     );
   }
--- site/src/components/cards/Skills.js
+++ site/src/components/cards/Skills.js
@@ -1,27 +1,33 @@
 import React, { Component } from "react";
-import { ListGroup } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
 export default class Skills extends Component {
   render() {
     return (
-      <div style={{ width: "" }}>
+      <div style={{ width: "", padding: 10 }}>
         <h1 style={{ textAlign: "center", color: "white", paddingBottom: 20 }}>
           Skills{" "}
           <span>
             <i class="fas fa-hammer"></i>
           </span>
         </h1>
-        <div style={{ textAlign: "center" }}>
+        <div style={{ textAlign: "center", fontWeight: "bold" }}>
           <p style={{ color: "white" }}>
-            Languages: JavaScript, C, Python, PHP, HTML/CSS
+            <span style={{ color: "#00FF00" }}>Languages:</span> JavaScript, C,
+            Python, PHP, HTML/CSS
           </p>
-          <p style={{ color: "white" }}>Frameworks: React Native, VueJS</p>
-          <p style={{ color: "white" }}>Libraries: ReactJS, Redux</p>
+          <p style={{ color: "white" }}>
+            <span style={{ color: "#00FF00" }}>Frameworks:</span> React Native,
+            VueJS
+          </p>
+          <p style={{ color: "white" }}>
+            <span style={{ color: "#00FF00" }}>Libraries:</span> ReactJS, Redux
+          </p>
           <p style={{ color: "white" }}>
             MongoDB, MERN Stack apps, REST API, GraphQL
           </p>
           <p style={{ color: "white" }}>
-            Agile Development Methodogy, Linux/ubuntu , Server hosting
+            Agile Development Methodogy, Linux/Ubuntu , Server hosting
           </p>
         </div>
       </div>
