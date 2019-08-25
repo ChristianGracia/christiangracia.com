50b2fe96385db678351227b7116919a25eb646d4
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-25 01:07:26 -0400

    added routing

--- site/src/App.js
+++ site/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Jumbotron from "./components/Jumbotron";
 import Content from "./components/Content";
 import Header from "./components/Header";
+import Work from "./components/views/Work";
 
 import "./App.css";
 
@@ -10,8 +12,19 @@ function App() {
     <div className="App" style={{ backgroundColor: "#343A40" }}>
       <Header />
       <Jumbotron />
-
       <Content />
+      <Router>
+        <Route exact path="/work" component={Work} />
+      </Router>
+      <Router>
+        <Route exact path="/about" component={Work} />
+      </Router>
+      <Router>
+        <Route exact path="/contact" component={Work} />
+      </Router>
+      <Router>
+        <Route exact path="/home" component={Work} />
+      </Router>
 
       <p>writing this site when I have time</p>
       <p>@lil_cuffy</p>
--- site/src/components/Header.js
+++ site/src/components/Header.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-import { Nav, Navbar, FormControl } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 
 export default class Header extends Component {
   render() {
     return (
       <div style={{}}>
         <Navbar style={{ justifyContent: "right" }} variant="dark" bg="dark">
-          <Navbar.Brand href="#home">Home</Navbar.Brand>
+          <Navbar.Brand href="/">Home</Navbar.Brand>
           <Nav className="ml-auto">
-            <Nav.Link href="#home">Work</Nav.Link>
+            <Nav.Link href="/work">Projects</Nav.Link>
             <Nav.Link href="#features">About</Nav.Link>
             <Nav.Link href="#pricing">Contact</Nav.Link>
           </Nav>
