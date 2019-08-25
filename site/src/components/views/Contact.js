e4ff39cfd0273b4721963d5f468e4c85378b9b4b
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-25 13:57:09 -0400

    gonna push updates

--- site/src/components/views/Contact.js
+++ site/src/components/views/Contact.js
@@ -3,8 +3,9 @@ import React, { Component } from "react";
 export default class Contact extends Component {
   render() {
     return (
-      <div>
+      <div className="joke" style={{}}>
         <h1 style={{ textAlign: "center" }}>TODO</h1>
+
         <div style={{ paddingBottom: 1000 }}></div>
       </div>
     );
