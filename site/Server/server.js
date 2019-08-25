d5918fe18568248631521e6e4c617e8b3b37e207
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-25 11:10:45 -0400

    attempting back end

---
+++ site/Server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
+});
+
+const port = process.env.PORT || 80;
+app.listen(port);
+
+console.log(`listening on ${port}`);
