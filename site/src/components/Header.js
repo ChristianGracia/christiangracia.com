6584bc12470942b4f9f57d5aa423506c067e6b58
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-08 23:32:49 -0400

    added hook for interval use, trying to get to work

--- site/src/components/Header.js
+++ site/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Nav, Navbar } from "react-bootstrap";
+import { IntervalHook } from "./IntervalHook";
 
 export default class Header extends Component {
   constructor(props) {
@@ -167,6 +168,7 @@ export default class Header extends Component {
               ></i>
             )}
           </Navbar.Brand>
+          <IntervalHook />
           <Nav className="ml-auto">
             <Nav.Link href="/work" style={{ color: "white" }}>
               Projects
---
+++ site/src/components/IntervalHook.js
@@ -0,0 +1,138 @@
+import React, { useState, useEffect } from "react";
+
+function IntervalHook() {
+  const [iconIndex, setIconIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIconIndex(
+        iconIndex => (iconIndex + Math.floor(Math.random() * 20)) % 20
+      );
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const icons = [
+    "fas fa-fan fa-pulse",
+    "fab fa-ello fa-pulse",
+    "fas fa-cookie fa-pulse",
+    "fab fa-cloudsmith fa-pulse",
+    "fab fa-codepen fa-pulse",
+    "fab fa-codepen fa-pulse",
+    "fab fa-codepen fa-pulse",
+    "fab fa-codepen fa-pulse",
+    "fab fa-codepen fa-pulse",
+    "fab fa-codepen fa-pulse",
+    "fab fa-codepen fa-pulse",
+    "fab fa-codepen fa-pulse",
+    "fab fa-codepen fa-pulse",
+    "fab fa-codepen fa-pulse",
+    "fas fa-clock fa-pulse",
+    "fas fa-clock fa-pulse",
+    "fas fa-clock fa-pulse",
+    "far fa-clock fa-pulse",
+    "far fa-clock fa-pulse",
+    "far fa-clock fa-pulse",
+    "far fa-clock fa-pulse",
+    "fas fa-clock fa-pulse",
+    "fas fa-clock fa-pulse",
+    "fas fa-clock fa-pulse",
+    "far fa-clock fa-pulse",
+    "far fa-clock fa-pulse",
+    "far fa-clock fa-pulse",
+    "far fa-clock fa-pulse",
+    "fas fa-circle-notch fa-pulse",
+    "fas fa-circle-notch fa-pulse",
+    "fas fa-circle-notch fa-pulse",
+    "fas fa-atom fa-pulse",
+    "fas fa-atom fa-pulse",
+    "fas fa-atom fa-pulse",
+    "fas fa-atom fa-pulse",
+    "fas fa-atom fa-pulse",
+    "fas fa-dice-d20 fa-pulse",
+    "fas fa-adjust fa-pulse",
+    "fas fa-star fa-pulse",
+    "fas fa-adjust fa-pulse",
+    "fas fa-star fa-pulse",
+    "fas fa-adjust fa-pulse",
+    "fas fa-star fa-pulse",
+    "fas fa-compress fa-pulse",
+    "fas fa-arrows-alt fa-pulse",
+    "fas fa-arrows-alt fa-pulse",
+    "fas fa-arrows-alt fa-pulse",
+    "fas fa-arrows-alt fa-pulse",
+    "fas fa-arrows-alt fa-pulse",
+    "fas fa-arrows-alt fa-pulse",
+    "fas fa-asterisk fa-pulse",
+    "fas fa-bacon fa-pulse",
+    "fas fa-egg fa-pulse",
+    "fas fa-ellipsis-v fa-pulse",
+    "fab fa-dyalog fa-pulse",
+    "fas fa-ellipsis-h fa-pulse",
+    "fas fa-fighter-jet fa-pulse",
+    "fas fa-fighter-jet fa-pulse",
+    "fas fa-fighter-jet fa-pulse",
+    "fas fa-eye fa-pulse",
+    "far fa-eye fa-pulse",
+    "fas fa-eye fa-pulse",
+    "far fa-eye fa-pulse",
+    "fas fa-external-link-alt fa-pulse",
+    "fas fa-exclamation-triangle fa-pulse",
+    "fas fa-fire-alt fa-pulse",
+    "fas fa-fire fa-pulse",
+    "fas fa-fire-alt fa-pulse",
+    "fas fa-fire fa-pulse",
+    "fas fa-ghost fa-pulse",
+    "far fa-gem fa-pulse",
+    "far fa-gem fa-pulse",
+    "fas fa-globe-africa fa-pulse",
+    "fas fa-globe-americas fa-pulse",
+    "fas fa-globe-americas fa-pulse",
+    "fas fa-globe-americas fa-pulse",
+    "fas fa-globe-americas fa-pulse",
+    "fas fa-globe-americas fa-pulse",
+    "fas fa-globe-europe fa-pulse",
+    "fas fa-globe-asia fa-pulse",
+
+    "fab fa-hotjar fa-pulse",
+    "fab fa-hubspot fa-pulse",
+    "fab fa-hubspot fa-pilse",
+    "fas fa-haykal fa-spin",
+    "fas fa-fan fa-pulse",
+    "fab fa-ello fa-pulse",
+    "fas fa-cookie fa-pulse",
+    "fab fa-cloudsmith fa-pulse",
+    "fab fa-codepen fa-pulse",
+    "fas fa-clock fa-pulse",
+    "far fa-clock fa-pulse",
+    "fas fa-circle-notch fa-pulse",
+    "fas fa-atom fa-pulse",
+    "fas fa-dice-d20 fa-pulse",
+    "fas fa-adjust fa-pulse",
+    "fas fa-star fa-pulse",
+    "fas fa-compress fa-pulse",
+    "fas fa-arrows-alt fa-pulse",
+    "fas fa-asterisk fa-pulse",
+    "fas fa-bacon fa-pulse",
+    "fas fa-bacon fa-pulse",
+    "fas fa-bacon fa-pulse",
+    "fas fa-bacon fa-pulse",
+    "fas fa-bacon fa-pulse",
+    "fas fa-bacon fa-pulse",
+    "fas fa-bacon fa-pulse",
+    "fas fa-bacon fa-pulse",
+    "fas fa-bacon fa-pulse",
+    "fas fa-bacon fa-pulse",
+    "fas fa-bacon fa-pulse",
+    "fas fa-bacon fa-pulse",
+    "fas fa-bacon fa-pulse",
+    "fas fa-bacon fa-pulse"
+  ];
+
+  return (
+    <div>
+      <span>{icons[iconIndex]}</span>
+    </div>
+  );
+}
+export default IntervalHook;
