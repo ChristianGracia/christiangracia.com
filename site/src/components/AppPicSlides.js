ae3882f45f782344486fa38f399e37113a9adff5
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-21 02:35:44 -0400

    huge update, added current projects and carosel components

---
+++ site/src/components/AppPicSlides.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+
+export default function AppPickSlides() {
+  return (
+    <div style={{}}>
+      <Carousel style={{}}>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://user-images.githubusercontent.com/42984214/64983893-96985780-d88f-11e9-9ad5-61490af4d474.png"
+            alt="First slide"
+            size="contain"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://user-images.githubusercontent.com/42984214/64983895-97c98480-d88f-11e9-9246-f0ac636ab4a2.png"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://user-images.githubusercontent.com/42984214/64983892-95672a80-d88f-11e9-811e-1711c5870bb0.png"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+}
--- site/src/components/CurrentProjects.js
+++ site/src/components/CurrentProjects.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+import AppPickSlides from "./AppPicSlides";
 
 export default function CurrentProjects() {
   return (
-    <div style={{ flex: 1, alignItems: "center" }}>
+    <div style={{ flex: 1, alignItems: "center", textAlign: "center" }}>
       <p
         style={{
           fontWeight: "bold",
@@ -14,7 +15,69 @@ export default function CurrentProjects() {
       >
         Some of my current projects
       </p>
+      <p style={{ textAlign: "center", color: "white", marginTop: 25 }}>
+        iOS/Android App for{" "}
+        <a style={{ color: "#FEFF01" }} href="http://adcofnorton.com">
+          adcofnorton.com
+        </a>
+      </p>
       <p style={{ textAlign: "center", color: "white" }}>
+        Over 11,000+ lines of React Native code
+      </p>
+      <p style={{ textAlign: "center", color: "white", fontWeight: "bold" }}>
+        code:{" "}
+        <a
+          href="https://github.com/ChristianGracia/Dental-iOSApp-React-Native"
+          style={{ color: "#FEFF01" }}
+        >
+          Link
+        </a>
+      </p>
+      <div
+        className=""
+        style={{
+          width: "70vw",
+          heigth: "400px",
+          maxWidth: 500,
+          margin: "0 auto"
+        }}
+      >
+        <AppPickSlides />
+      </div>
+
+      <i
+        style={{
+          textAlign: "center",
+          color: "#FEFF01",
+          fontSize: 25,
+          marginTop: 25
+        }}
+        class="fas fa-code"
+      ></i>
+
+      <p style={{ textAlign: "center", color: "white", marginTop: 25 }}>
+        Luxury Clothes website
+        <a style={{ color: "#FEFF01" }} href="https://cuffy-inc.herokuapp.com/">
+          {" "}
+          cuffy-inc.herokuapp.com
+        </a>
+      </p>
+      <p style={{ textAlign: "center", color: "white" }}>
+        In progess, enterprise level site with React, Redux, Sass, GraphQl and
+        more
+      </p>
+
+      <i
+        style={{
+          textAlign: "center",
+          color: "#FEFF01",
+          fontSize: 25,
+          marginTop: 25
+        }}
+        class="fas fa-code"
+      ></i>
+
+      <p style={{ textAlign: "center", color: "white", marginTop: 25 }}>
         Rewriting{" "}
         <a style={{ color: "#FEFF01" }} href="http://onabeat.com">
           {" "}
@@ -25,7 +88,12 @@ export default function CurrentProjects() {
         Not live yet, due to launch in a month or two
       </p>
 
-      <p style={{ textAlign: "center", color: "white" }}>
+      <i
+        style={{ textAlign: "center", color: "#FEFF01", fontSize: 25 }}
+        class="fas fa-code"
+      ></i>
+
+      <p style={{ textAlign: "center", color: "white", marginTop: 25 }}>
         Rewriting{" "}
         <a style={{ color: "#FEFF01" }} href="http://nflandscaping.com">
           nflandscaping.com
@@ -34,13 +102,13 @@ export default function CurrentProjects() {
       <p style={{ textAlign: "center", color: "white" }}>
         Also not live. Waiting on domain access to post what I have written
       </p>
-      <p style={{ textAlign: "center", color: "white" }}>
+      <p style={{ textAlign: "center", color: "white", fontWeight: "bold" }}>
         code:{" "}
         <a
-          href=" https://github.com/ChristianGracia/nflandscaping.com"
+          href="https://github.com/ChristianGracia/nflandscaping.com"
           style={{ color: "#FEFF01" }}
         >
-          Code Link
+          Link
         </a>
       </p>
     </div>
