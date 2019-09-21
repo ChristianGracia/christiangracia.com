307709832d210422b818de04be33e25215a0acd6
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-21 02:50:42 -0400

    last updates for redeploy

--- site/src/components/CurrentProjects.js
+++ site/src/components/CurrentProjects.js
@@ -17,18 +17,22 @@ export default function CurrentProjects() {
       </p>
       <p style={{ textAlign: "center", color: "white", marginTop: 25 }}>
         iOS/Android App for{" "}
-        <a style={{ color: "#FEFF01" }} href="http://adcofnorton.com">
+        <a
+          style={{ color: "#FEFF01", fontWeight: "bold" }}
+          href="http://adcofnorton.com"
+        >
           adcofnorton.com
         </a>
+        .
       </p>
       <p style={{ textAlign: "center", color: "white" }}>
-        Over 11,000+ lines of React Native code
+        Over 11,000+ lines of React Native code.
       </p>
       <p style={{ textAlign: "center", color: "white", fontWeight: "bold" }}>
         code:{" "}
         <a
           href="https://github.com/ChristianGracia/Dental-iOSApp-React-Native"
-          style={{ color: "#FEFF01" }}
+          style={{ color: "#FEFF01", fontWeight: "bold" }}
         >
           Link
         </a>
@@ -57,14 +61,27 @@ export default function CurrentProjects() {
 
       <p style={{ textAlign: "center", color: "white", marginTop: 25 }}>
         Luxury Clothes website
-        <a style={{ color: "#FEFF01" }} href="https://cuffy-inc.herokuapp.com/">
+        <a
+          style={{ color: "#FEFF01", fontWeight: "bold" }}
+          href="https://cuffy-inc.herokuapp.com/"
+        >
           {" "}
           cuffy-inc.herokuapp.com
         </a>
       </p>
       <p style={{ textAlign: "center", color: "white" }}>
         In progess, enterprise level site with React, Redux, Sass, GraphQl and
-        more
+        more. Currently looks great on a computer, working on mobile.
+      </p>
+
+      <p style={{ textAlign: "center", color: "white", fontWeight: "bold" }}>
+        code:{" "}
+        <a
+          href="https://github.com/ChristianGracia/Cuffy-Professional-Store-Site"
+          style={{ color: "#FEFF01" }}
+        >
+          Link
+        </a>
       </p>
 
       <i
@@ -79,13 +96,16 @@ export default function CurrentProjects() {
 
       <p style={{ textAlign: "center", color: "white", marginTop: 25 }}>
         Rewriting{" "}
-        <a style={{ color: "#FEFF01" }} href="http://onabeat.com">
+        <a
+          style={{ color: "#FEFF01", fontWeight: "bold" }}
+          href="http://onabeat.com"
+        >
           {" "}
           onabeat.com
         </a>
       </p>
       <p style={{ textAlign: "center", color: "white" }}>
-        Not live yet, due to launch in a month or two
+        Not live yet, due to launch in a month or two.
       </p>
 
       <i
@@ -95,12 +115,15 @@ export default function CurrentProjects() {
 
       <p style={{ textAlign: "center", color: "white", marginTop: 25 }}>
         Rewriting{" "}
-        <a style={{ color: "#FEFF01" }} href="http://nflandscaping.com">
+        <a
+          style={{ color: "#FEFF01", fontWeight: "bold" }}
+          href="http://nflandscaping.com"
+        >
           nflandscaping.com
         </a>
       </p>
       <p style={{ textAlign: "center", color: "white" }}>
-        Also not live. Waiting on domain access to post what I have written
+        Also not live. Waiting on domain access to post what I have written.
       </p>
       <p style={{ textAlign: "center", color: "white", fontWeight: "bold" }}>
         code:{" "}
