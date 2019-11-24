95710844f9310bc655126c898cf059c16a2f7a3f
Christian Gracia <christiangracia.student@careerdevs.com>
2019-11-24 04:43:46 -0500

    github turned off for rate limit

--- site/src/components/github/github-component.jsx
+++ site/src/components/github/github-component.jsx
@@ -19,7 +19,7 @@ class Github extends Component {
   componentDidMount() {
     const { count, sort, clientId, clientSecret } = this.state;
     const url = `https://api.github.com/users/ChristianGracia/repos?per_page=${count}&sort=${sort}&client_id=${clientId}&client_secret=${clientSecret}`;
-    fetch(url)
+    fetch(url, { accept: "application/vnd.github.v3+json" })
       .then(res => res.json())
       .then(data => {
         if (this.refs.myRef) {
@@ -31,45 +31,45 @@ class Github extends Component {
   render() {
     const { repos } = this.state;
 
-    const repoItems = repos.map(repo => (
-      <div key={repo.id} style={{}}>
-        <Fade top>
-          <div
-            key={repo.id}
-            className="card card-body mb-2 bg-light ml-auto mr-auto pr-3"
-            style={{ margin: 0, paddingRight: 0, width: "88vw" }}
-          >
-            <div className="row">
-              <div className="col-lg-6" style={{}}>
-                <h4 style={{ color: "white" }}>
-                  <Link
-                    to={`//${repo.html_url.substr(7)}`}
-                    className="text-info"
-                    target="_blank"
-                  >
-                    <p style={{ color: "#3993EC" }}>{repo.name}</p>
-                  </Link>
-                </h4>
-                <p>{repo.description}</p>
-              </div>
+    // const repoItems = repos.map(repo => (
+    //   <div key={repo.id} style={{}}>
+    //     <Fade top>
+    //       <div
+    //         key={repo.id}
+    //         className="card card-body mb-2 bg-light ml-auto mr-auto pr-3"
+    //         style={{ margin: 0, paddingRight: 0, width: "88vw" }}
+    //       >
+    //         <div className="row">
+    //           <div className="col-lg-6" style={{}}>
+    //             <h4 style={{ color: "white" }}>
+    //               <Link
+    //                 to={`//${repo.html_url.substr(7)}`}
+    //                 className="text-info"
+    //                 target="_blank"
+    //               >
+    //                 <p style={{ color: "#3993EC" }}>{repo.name}</p>
+    //               </Link>
+    //             </h4>
+    //             <p>{repo.description}</p>
+    //           </div>
 
-              <div className="col-md-6" style={{}}>
-                <span className="badge badge-info mr-1">
-                  <i class="fas fa-star"></i> Stars: {repo.stargazers_count}
-                </span>
-                <span className="badge badge-secondary mr-1">
-                  <i class="fas fa-eye"></i> Watchers: {repo.watchers_count}
-                </span>
-                <span className="badge badge-success">
-                  <i class="fas fa-code-branch"></i> Forks: {repo.forks_count}
-                </span>
-              </div>
-            </div>
-          </div>
-        </Fade>
-        <div style={{ padding: 30 }}></div>
-      </div>
-    ));
+    //           <div className="col-md-6" style={{}}>
+    //             <span className="badge badge-info mr-1">
+    //               <i class="fas fa-star"></i> Stars: {repo.stargazers_count}
+    //             </span>
+    //             <span className="badge badge-secondary mr-1">
+    //               <i class="fas fa-eye"></i> Watchers: {repo.watchers_count}
+    //             </span>
+    //             <span className="badge badge-success">
+    //               <i class="fas fa-code-branch"></i> Forks: {repo.forks_count}
+    //             </span>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </Fade>
+    //     <div style={{ padding: 30 }}></div>
+    //   </div>
+    // ));
 
     return (
       <div ref="myRef">
@@ -142,7 +142,7 @@ class Github extends Component {
           {this.state.showRepo ? (
             <div style={{ justifyContent: "center", alignItems: "center" }}>
               {" "}
-              {repoItems}
+              {/* {repoItems} */}
             </div>
           ) : null}
         </div>
