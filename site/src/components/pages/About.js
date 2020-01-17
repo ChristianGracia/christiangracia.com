4405ec7075b0a4e3c9c7fa7e81dc5eed88b9158d
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-17 02:48:15 -0500

    added site functional component to displays sites I built and manage

--- site/src/components/pages/About.js
+++ site/src/components/pages/About.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import SkillsAndEdu from "../skills-and-edu/skills-and-edu.component";
+import Sites from "../sites/sites.components";
 
 export default class About extends Component {
   render() {
     return (
       <div>
+        <Sites />
         <SkillsAndEdu />
         <div style={{ padding: 40 }}></div>
       </div>
---
+++ site/src/components/sites/sites.components.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Sites = () => {
+  return (
+    <div>
+      <span>Sites I built and host</span>
+      <span>
+        <a href="https://www.onabeat.com">onabeat.com</a>
+      </span>
+      <span>
+        <a href="https://www.nflandscaping.com">nflandscaping.com</a>
+      </span>
+    </div>
+  );
+};
+export default Sites;
