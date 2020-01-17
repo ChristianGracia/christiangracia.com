297e39e71003c42f7af290f9de84ce9863c9bd93
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-17 03:11:10 -0500

    styled site flex component

--- site/src/components/sites/sites.components.jsx
+++ site/src/components/sites/sites.components.jsx
@@ -1,36 +1,41 @@
 import React from "react";
 import "./sites.styles.css";
+import Fade from "react-reveal/Fade";
 
 const Sites = () => {
   return (
-    <div className="sites-container">
-      <span className="sites-header">Sites I built and host</span>
-      <span>
-        <a className="site-link" href="https://www.onabeat.com">
-          onabeat.com
-        </a>
-      </span>
-      <span>
+    <Fade left cascade>
+      <div className="sites-container">
+        <span className="sites-header">Sites I built and host</span>
+        <span className="site-div">
+          <a className="site-link" href="https://www.onabeat.com">
+            onabeat.com
+          </a>
+        </span>
+        <span className="site-div" style={{ marginTop: 30 }}>
+          <a className="site-link" href="https://www.nflandscaping.com">
+            nflandscaping.com
+          </a>
+        </span>
+        <span className="sites-detail">
+          This site will be up soon. On a slow load server for now so it takes a
+          min or two to load until we buy host
+        </span>
+        <span className="site-div">
+          <a className="site-link" href="https://crane-site.herokuapp.com/">
+            crane-site.herokuapp.com/
+          </a>
+        </span>
+        {/* <span className="site-div">
         <a className="site-link" href="https://www.nflandscaping.com">
           nflandscaping.com
         </a>
-      </span>
-
-      <span>On a slow load server</span>
-      <span>Takes 2 mins to load will by host soon</span>
-
-      <span>
-        <a className="site-link" href="https://www.nflandscaping.com">
-          nflandscaping.com
-        </a>
-      </span>
-
-      <span>
-        <a className="site-link" href="https://www.nflandscaping.com">
-          nflandscaping.com
-        </a>
-      </span>
-    </div>
+      </span> */}
+        <span style={{ fontWeight: "bold", color: "green" }}>
+          All sites are paid
+        </span>
+      </div>
+    </Fade>
   );
 };
 export default Sites;
