90d3da57cd9c1d99dfb8d003f0840e49a2e5d9f0
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-20 23:19:12 -0500

    made links buttons with bootstrap class

--- site/src/components/sites/sites.components.jsx
+++ site/src/components/sites/sites.components.jsx
@@ -8,16 +8,22 @@ const Sites = () => {
       <div className="sites-container">
         <span className="sites-header">Sites I built and host</span>
         <span className="site-div">
-          <a className="site-link" href="https://www.onabeat.com">
+          <a
+            className="site-link btn btn-danger"
+            href="https://www.onabeat.com"
+          >
             onabeat.com
           </a>
         </span>
-        <span className="site-div" style={{ marginTop: 30 }}>
-          <a className="site-link" href="https://www.nflandscaping.com">
+        <span className="site-div" style={{ marginTop: 30, marginBottom: 30 }}>
+          <a
+            className="site-link btn btn-danger"
+            href="https://www.nflandscaping.com"
+          >
             nflandscaping.com
           </a>
         </span>
-        <span className="sites-detail">
+        {/* <span className="sites-detail">
           This site will be up soon. On a slow load server for now so it takes a
           min or two to load until we buy host
         </span>
@@ -30,7 +36,7 @@ const Sites = () => {
           <a className="site-link" href="http://www.christiangracia.com">
             insurance site coming soon(weeks)
           </a>
-        </span>
+        </span> */}
         <span
           style={{
             fontWeight: "bold",
