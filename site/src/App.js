d60ff89325aed0fa6c1a381b8998edea291f2f21
Christian Gracia <christiangracia.student@careerdevs.com>
2020-02-08 18:44:38 -0500

    renamed header with the way I like to style components now

--- site/src/App.js
+++ site/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Header from "./components/nav/Header";
+import Header from "./components/nav/header.component.jsx";
 import Work from "./components/pages/work-page/work-page.component";
 import About from "./components/pages/about-page/about-page.component";
 import Home from "./components/pages/home-page/home-page.component";
