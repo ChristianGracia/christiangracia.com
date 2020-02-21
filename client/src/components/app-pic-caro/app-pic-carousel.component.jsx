5292654c950b5ae3a492ec0149303da36fa8d385
Christian Gracia <christiangracia.student@careerdevs.com>
2020-02-20 22:38:59 -0500

    converted some css sheets to sass scss

--- client/src/components/app-pic-caro/app-pic-carousel.component.jsx
+++ client/src/components/app-pic-caro/app-pic-carousel.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
-import "./app-pic-carousel.styles.css";
+import "./app-pic-carousel.styles.scss";
 
 export default function AppPicSlides() {
   return (
--- client/src/components/common/project-box/project-box.component.jsx
+++ client/src/components/common/project-box/project-box.component.jsx
@@ -1,40 +1,48 @@
 import React from "react";
 import { TwitterTweetEmbed } from "react-twitter-embed";
-import "./project-box.styles.css"
+import "./project-box.styles.scss";
 import { Button } from "react-bootstrap";
 
 const ProjectBox = ({ description, title, codeLink, tweet, siteLink }) => {
-    return (<div style={{ display: "flex", justifyContent: "center", flexDirection: "column", textAlign: "center" }}>
-
-        <p className="title">
-            <a
-                className="linkStyle"
-                href={siteLink}
-            >
-                <span>{title}</span>
-            </a>
-        </p>
-        <p className="description" style={{ maxWidth: 900 }}>
-            {description}
-        </p>
-        <div style={{ marginTop: 30, marginBottom: 30 }}>
-            <Button onClick={() => window.location.href = codeLink} variant="danger">Link to Code</Button>
-        </div>
-
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "center"
-            }}
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        textAlign: "center"
+      }}
+    >
+      <p className="title">
+        <a className="linkStyle" href={siteLink}>
+          <span>{title}</span>
+        </a>
+      </p>
+      <p className="description" style={{ maxWidth: 900 }}>
+        {description}
+      </p>
+      <div style={{ marginTop: 30, marginBottom: 30 }}>
+        <Button
+          onClick={() => (window.location.href = codeLink)}
+          variant="danger"
         >
-            <div style={{ minWidth: 290, maxWidth: 700 }}>
-                {tweet === "" ? null :
-                    <TwitterTweetEmbed
-                        options={{ width: 300 }}
-                        tweetId={tweet}
-                    />}
-            </div>
+          Link to Code
+        </Button>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center"
+        }}
+      >
+        <div style={{ minWidth: 290, maxWidth: 700 }}>
+          {tweet === "" ? null : (
+            <TwitterTweetEmbed options={{ width: 300 }} tweetId={tweet} />
+          )}
         </div>
-    </div>)
-}
+      </div>
+    </div>
+  );
+};
 export default ProjectBox;
